139ab1f99f6af8699a5448e969ccc98f922b0bb5
joohwan <joohwan9607@mobility42.io>
2023-04-26 19:09:19 +0900

    양방향 연관관계

--- src/main/java/com/hellojpa/Member.java
+++ src/main/java/com/hellojpa/Member.java
@@ -7,51 +7,10 @@ public class Member {
     @Id
     @GeneratedValue
     @Column(name = "MEMBER_ID")
-    private String id;
-    @Column(name = "name", nullable = false)
-    private String name;
-
-    private String city;
-    private String street;
-    private String zipcode;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
+    private Long id;
+    @Column(name = "USERNAME", nullable = false)
+    private String username;
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 }
---
+++ src/main/java/com/hellojpa/Team.java
@@ -0,0 +1,16 @@
+package com.hellojpa;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue
+    @Column(name = "TEAM_ID")
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+}
